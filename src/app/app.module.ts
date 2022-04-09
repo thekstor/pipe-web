6c26104f89be2e027ecf7c1a6bd1460e49799d59
Felipe Medina <felipe.medina@car1.cl>
2022-04-09 16:38:11 -0400

    armando camino

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainPageModule } from './pages/main-page/main-page.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MainPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
