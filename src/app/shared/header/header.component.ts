71364a46ad87b7367fbbfc586a66fcd017b041c3
Felipe Medina <felipe.medina@car1.cl>
2022-04-12 18:20:53 -0400

    orden de componentes platillas y fuentes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isDark: boolean = this.isDarkValue();
+  isNavbarCollapsed=true;
+
+  constructor() {
+    if(this.isDark) {
+      document.documentElement.className = 'dark';
+    }
+   }
 
   ngOnInit(): void {
   }
 
+  changeTheme(): void {
+    document.documentElement.className = this.isDark ? 'light' : 'dark';
+    this.isDark = !this.isDark;
+  }
+  isDarkValue(): boolean {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
 }
